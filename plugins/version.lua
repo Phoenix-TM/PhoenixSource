856b6d92bc76b6606e6996e557feff7da9e8df47
Nima1383 <nimaboghrati@yahoo.com>
2016-07-05 13:20:04 +0430

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,26 @@
+do
+
+function run(msg, matches)
+  return 'PhoenixTG Bot v1'.. [[ 
+  
+  Anti Spam Bot : @PhoenixTG
+  Channel : @Phoenix_TM
+  
+  Admins :
+  @NR_Offical [Founder]
+  @Nim_Bo [Manager & Developer] 
+  ]]
+end
+
+return {
+  description = "Robot and Creator About", 
+  usage = "/version : robot info",
+  patterns = {
+    "^[!/#][Vv][Ee][Rr][Ss][Ii][Oo][NN]$"
+  }, 
+  run = run 
+}
+
+end
+
+-- @Phoenix_TM
